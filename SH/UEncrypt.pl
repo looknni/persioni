05b049b75a6f0a31544c6a5eaa48d113e4ac855c
looknni <looknnniii@outlook.com>
2023-04-05 10:23:21 +0800

    ><

--- SH/UEncrypt.pl
+++ SH/UEncrypt.pl
@@ -4,7 +4,58 @@ print "help: perl $0 '\@ARGV[0]'\n\@ARGV[0],'string'\n\n";
 =pod
 This is perl 5, version 36, subversion 0 (v5.36.0) built for
 =cut
-my @dicsk=('A'..'Z','a'..'z',0..99);
+my @dicsk=('A'..'Z','a'..'z',0..99,'AA'..'ZZ','aA'..'zZ','Aa'..'Zz');
+foreach (100..41008){
+push @dicsk,$_;
+}
+foreach ('AAA'..'ZZZ'){
+push @dicsk,$_;
+}
+foreach (41009..50018){
+push @dicsk,$_;
+}
+foreach ('AAa'..'ZZz'){
+push @dicsk,$_;
+}
+foreach (50019..61008){
+push @dicsk,$_;
+}
+foreach ('AaA'..'ZzZ'){
+push @dicsk,$_;
+}
+foreach (61009..70028){
+push @dicsk,$_;
+}
+foreach ('aAA'..'zZZ'){
+push @dicsk,$_;
+}
+foreach (70029..80038){
+push @dicsk,$_;
+}
+foreach ('aaA'..'zzZ'){
+push @dicsk,$_;
+}
+foreach (80039..90018){
+push @dicsk,$_;
+}
+foreach ('aAa'..'zZz'){
+push @dicsk,$_;
+}
+foreach (90019..100108){
+push @dicsk,$_;
+}
+foreach ('Aaa'..'Zzz'){
+push @dicsk,$_;
+}
+foreach (100109..110098){
+push @dicsk,$_;
+}
+foreach ('aaa'..'zzz'){
+push @dicsk,$_;
+}
+foreach (110099..125212){
+push @dicsk,$_;
+}
 my %codek;
 for($a=0;$a<scalar(@dicsk);$a++){
         $codek{$dicsk[$a]} = "$a";
