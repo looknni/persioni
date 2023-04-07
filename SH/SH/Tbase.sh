012992e9eb1134e787bf9d1fb4676d2211282b0a
looknni <looknnniii@outlook.com>
2023-04-07 09:32:04 +0800

    ><

--- SH/SH/Tbase.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-zase=({a..z} {A..Z} {0..9} '/' '#')
-for nm in ${zase[@]} ;do
-echo -n "$nm "
-echo $nm |md5sum|base32
-echo $nm |md5sum|base64
-echo $nm |md5sum|base32|base64
-echo $nm |md5sum|base64|base32
-done
