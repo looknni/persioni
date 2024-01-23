40dc832f4dcb44eb37fe4f288611931f7a3ea233
looknnn <g@g.org>
2024-01-23 16:39:44 +0800

    #

--- atob.bash
+++ atob.bash
@@ -5,10 +5,16 @@ set -x
 for i in `seq 0 $num`;do
 	cp $1 "f-"$i
 done
+#
 a=`cat $2`
 b=0
 for j in $a;do
-	sed -i "s/\$/$j/g" 'f-'$((b++))
+	sed -i "s/^/$j/g" 'f-'$((b++))
 	echo $b
+done
+#
+for k in {1..9};do
+        cat f-${k}* > m2n${k}
+        rm f-${k}*
 done
 set +x
