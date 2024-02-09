9f31332bd28e9aba008bd2e528634d43f7be3a4f
looknnn <g@g.org>
2024-02-09 09:21:03 +0800

    #

--- atob.bash
+++ atob.bash
@@ -1,19 +1,22 @@
 #!/bin/bash
-# $1,source $2,file
+# $1,source $2,file $3,name
+if [[ -z $1 || -z $2 || -z $3 ]];then
+	exit
+fi
 num=`wc $2|awk '{print $1}'`
 set -x
 for i in `seq 0 $num`;do
-	cp $1 "f-$i"
+	cp $1 "f-"$i
 done
 #
 a=`cat $2`
 b=0
 for j in $a;do
-	sed -i "s/^/$j/g" 'f-'$((b++))
+	sed -i "s/\$/$j/g" 'f-'$((b++))
 done
 #
 for k in {1..9};do
-        cat "f-${k}"* > "m2n${k}"
-        rm "f-${k}"*
+        cat "f-"${k}* > "$3"${k}
+        rm "f-"${k}*
 done
 set +x
