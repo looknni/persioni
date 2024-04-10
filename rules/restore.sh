2463cbaa2d43ef0064098c36dc393efaba9b7526
looknnn <g@g.org>
2024-04-10 20:29:15 +0800

    #

--- rules/restore.sh
+++ rules/restore.sh
@@ -5,7 +5,8 @@ restore6
 
 apo=$RANDOM
 if [ "$apo" -lt "2000" ];then
-	        apo=1234
-fi                                              uci set dhcp.@dnsmasq[0].port="$apo"
+	apo=1234
+fi
+uci set dhcp.@dnsmasq[0].port="$apo"
 uci commit dhcp
 service dnsmasq reload
