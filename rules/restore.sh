7315fe230b9861a22d743fc3295d369956c982e8
looknni <t@g.org>
2024-04-14 21:23:05 +0800

    #

--- rules/restore.sh
+++ rules/restore.sh
@@ -12,4 +12,4 @@ uci set dhcp.@dnsmasq[0].minport='1024'
 uci set dhcp.@dnsmasq[0].maxport='6000'
 uci commit dhcp
 service dnsmasq reload
-sed -i '1a\nameserver 114.114.114.114' /tmp/resolv.conf
+sed -i '3a\nameserver 114.114.114.114' /tmp/resolv.conf
