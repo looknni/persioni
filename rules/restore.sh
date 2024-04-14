920660527dd4ceb714c882fd0f3400b38be797ca
looknnn <g.g@g.o>
2024-04-14 11:39:02 +0800

    #

--- rules/restore.sh
+++ rules/restore.sh
@@ -8,5 +8,8 @@ if [ "$apo" -lt "2000" ];then
 	apo=1234
 fi
 uci set dhcp.@dnsmasq[0].port="$apo"
+uci set dhcp.@dnsmasq[0].minport='1024'
+uci set dhcp.@dnsmasq[0].maxport='6000'
 uci commit dhcp
 service dnsmasq reload
+sed -i '1a\nameserver 114.114.114.114' /tmp/resolv.conf
