0d58131c967115eb1a94a13885c90891386bebfc
looknni <t@g.org>
2024-04-29 14:25:54 +0800

    #

--- rules/restore.sh
+++ rules/restore.sh
@@ -7,6 +7,9 @@ apo=$RANDOM
 if [ "$apo" -lt "2000" ];then
 	apo=1234
 fi
+#wireless.default_radio1.multicast_to_unicast_all='1'
+#wireless.radio0.beacon_int='150'
+#wireless.radio1.beacon_int='100'
 uci set dhcp.@dnsmasq[0].port="$apo"
 uci set dhcp.@dnsmasq[0].minport='1024'
 uci set dhcp.@dnsmasq[0].maxport='6000'
