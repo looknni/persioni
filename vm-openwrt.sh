7b71e21a25de06f91f2a721a9095c14ac8a60937
looknni <looknnniii@outlook.com>
2024-07-01 20:35:04 +0800

    #

--- vm-openwrt.sh
+++ vm-openwrt.sh
@@ -36,7 +36,7 @@ ip tuntap del mode tap dev $LAN
 
 # ssh-copy-id -i ~/.ssh/id_rsa.pub -p 22 root@192.168.1.1 # /etc/dropbear/authorized_keys
 # ~/.ssh/config # ssh xxx
-# 	Host xxx
+# Host xxx
 #	Hostname 192.168.1.1
 #	IdentityFile ~/.ssh/id_rsa
 #	User root
