f0c5c274c346dbe7a1f8c7c8766609d6ec041964
looknni <looknnniii@outlook.com>
2025-01-19 20:28:02 +0800

    #

--- z.a.sh
+++ z.a.sh
@@ -9,17 +9,17 @@ set -x
 sed -i '/^$/d' $1
 sed -i '/^$/d' $2
 for i in `seq 1 $num`;do
-	cp $1 $pr$i
+	cp $1 ${pr}$i
 done
 #
 a=`cat $2`
 b=0
 for j in $a;do
-	sed -i "s/$3/$j/g" $pr$((++b))
+	sed -i "s/$3/${j}/g" ${pr}$((++b))
 done
 #
 for k in {1..9};do
-        cat $pr${k}* > "$4"${k}
-        rm $pr${k}*
+        cat ${pr}${k}* > "$4"${k}
+        rm ${pr}${k}*
 done
 set +x
