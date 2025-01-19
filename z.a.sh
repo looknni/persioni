66f8d8be9b1ecc73c678363baaa2bedc35496a17
looknni <looknnniii@outlook.com>
2025-01-19 21:57:21 +0800

    #

--- z.a.sh
+++ z.a.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# $1,source $2,file $3,name
-if [[ -z $3 ]];then
+# $1,source $2,file $3,^$ $4,name
+if [[ -z $4 ]];then
        exit
 fi
 num=`wc $2|awk '{print $1}'`
@@ -12,11 +12,11 @@ for i in `seq 1 $num`;do
 	cp $1 ${pr}$i
 done
 #
-a=`cat $2`
 b=0
-for j in $a;do
-	sed -i "s/$3/${j}/g" ${pr}$((++b))
-done
+while IFS= read -r line
+do
+	sed -i "s/$3/${line//&/\\&}/g" ${pr}$((++b))
+done < "$2"
 #
 for k in {1..9};do
         cat ${pr}${k}* > "$4"${k}
