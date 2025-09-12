00fc33e9da9a8b3c908ce3cc1bf92395c0dc0f5e
looknni <looknnniii@outlook.com>
2025-09-12 23:43:58 +0800

    #

--- openwrt-https.sh
+++
@@ -1,5 +0,0 @@
-n="uhttpd"
-openssl genrsa -out ${n}.key 1024
-openssl req -key ${n}.key -new -out ${n}.csr
-openssl x509 -signkey ${n}.key -in ${n}.csr -req -days 555 -out ${n}.crt -subj "/C=CN/ST=st/L=l/O=o/OU=ou/CN=cn/emailAddress=email@emil.com"
-openssl x509 -text -noout -in ${n}.crt
