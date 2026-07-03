1cc1d6035f28dcf58bbed1c2d9e72adba486dada
looknni <looknnniii@outlook.com>
2026-07-03 16:47:13 +0800

    #

--- vm-openwrt.sh
+++ vm-openwrt.sh
@@ -1,6 +1,25 @@
 # qemu-img create -f qcow2 xx.img 10G
 # qemu-system-x86_64 -m 2048 -smp 6 -enable-kvm xx.img -cdrom ./xx.iso
 
+#!/bin/sh
+IMAGE=./openwrt-22.03.5-x86-64-generic-ext4-combined.img
+LAN=openwrt_tap0
+# create tap interface which will be connected to OpenWrt LAN NIC
+ip tuntap add mode tap $LAN
+ip link set dev $LAN up
+# configure interface with static ip to avoid overlapping routes
+ip addr add 192.168.1.241/24 dev $LAN
+qemu-system-x86_64 \
+	-device virtio-net-pci,netdev=lan \
+	-netdev tap,id=lan,ifname=$LAN,script=no,downscript=no \
+	-device virtio-net-pci,netdev=wan \
+	-netdev user,id=wan $IMAGE
+	-M virt -nographic -m 512 -cpu cortex-a53 -smp 4 -kernel $IMAGE
+# cleanup. delete tap interface created earlier
+ip addr flush dev $LAN
+ip link set dev $LAN down
+ip tuntap del mode tap dev $LAN
+
 # /etc/rc.local # ip6tables-extra iptables-mod-iprange iptables-mod-filter iptables-mod-conntrack-extra iptables-mod-ipsec iptables-mod-nat-extra
 # iw phy phy0 interface add mon0 type monitor
 # timeout 1s tr -dc 'a-z0-9' < /dev/urandom |fold -w8 > 1
